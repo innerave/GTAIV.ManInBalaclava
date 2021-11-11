6fc2a65f20df593bd414958b74cef65fdffa809b
Alexander Vladimirov <innerave@gmail.com>
2021-11-11 10:05:15 +0300

    Added comment about isRequiredForMission.

--- ManInBalaclava/States/DrivingAway.cs
+++ ManInBalaclava/States/DrivingAway.cs
@@ -18,6 +18,11 @@ namespace ManInBalaclava.States
 		public void OnEnter()
 		{
 			NpcReaction.ReactingPed.Task.CruiseWithVehicle(NpcReaction.ReactingPed.CurrentVehicle, 40f, false);
+			// If you scare away some characters that were marked by the game as needed for the mission
+			// (for example, hot dog sellers and waiters),
+			// the game removes this flag after receiving a new task for the NPC
+			// This hack is needed to prevent this, and to keep tracking the NPC.
+			// Not a very good design, but hey, that's how the game works.
 			if (NpcReaction.NpcWasOriginallyAMissionCharacter)
 			{
 				NpcReaction.ReactingPed.isRequiredForMission = true;
--- ManInBalaclava/States/Fleeing.cs
+++ ManInBalaclava/States/Fleeing.cs
@@ -16,6 +16,11 @@ namespace ManInBalaclava.States
 		{
 			if (NpcReaction.ReactingPed.IsFleeing()) return;
 			NpcReaction.ReactingPed.Task.FleeFromChar(NpcReaction.Player, false);
+			// If you scare away some characters that were marked by the game as needed for the mission
+			// (for example, hot dog sellers and waiters),
+			// the game removes this flag after receiving a new task for the NPC
+			// This hack is needed to prevent this, and to keep tracking the NPC.
+			// Not a very good design, but hey, that's how the game works.
 			if (NpcReaction.NpcWasOriginallyAMissionCharacter)
 			{
 				NpcReaction.ReactingPed.isRequiredForMission = true;
